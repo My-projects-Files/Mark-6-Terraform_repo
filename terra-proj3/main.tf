72a2b72674c6bb4c903efa41ee6b0a8e902d7648
EC2 Default User <ec2-user@ip-172-31-28-11.ec2.internal>
2025-02-18 14:13:14 +0000

    main up

--- terra-proj3/main.tf
+++ terra-proj3/main.tf
@@ -1,17 +1,45 @@
-terraform {
-	provider "aws" {
+
+provider "aws" {
 	region = "us-east-1"  # Choose your region
-	}
+}
+# Create a VPC (if you don't already have one)
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
 
-	resource "aws_security_group" "rds_sg" {
-  		name        = "rds_security_group"
-  		description = "Allow inbound traffic to RDS from specific IPs"
+# Create subnets in your VPC
+resource "aws_subnet" "subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
 
-  		ingress {
-    			from_port   = 5432  # PostgreSQL port
-    			to_port     = 5432  # PostgreSQL port
-    			protocol    = "tcp"
-    			cidr_blocks = ["0.0.0.0/0"]  # Allow all IPs for simplicity, but you should restrict this in production
+resource "aws_subnet" "subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+# Create a DB Subnet Group
+resource "aws_db_subnet_group" "db_sub_group" {
+  name       = "db_sub_group"
+  subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+
+  tags = {
+    Name = "db_sub_group"
+  }
+}
+resource "aws_security_group" "rds_sg" {
+ 		name        = "rds_security_group"
+ 		description = "Allow inbound traffic to RDS from specific IPs"
+
+ 		ingress {
+   			from_port   = 5432  # PostgreSQL port
+   			to_port     = 5432  # PostgreSQL port
+   			protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]  # Allow all IPs for simplicity, but you should restrict this in production
   		}		
 
   		egress {
@@ -22,11 +50,11 @@ terraform {
   		}
 
   		tags = {
-   		Name = "RDS-SecurityGroup"
+   		  Name = "RDS-SecurityGroup"
   		}
-	}
+}
 	
-	resource "aws_db_instance" "default" {
+resource "aws_db_instance" "default" {
   		allocated_storage    = 10
   		engine               = "postgres"
   		engine_version       = "13.3"
@@ -34,23 +62,23 @@ terraform {
   		db_name              = "mydb"
   		username             = "mydbusr"
   		password             = "ad@123"
-  		db_subnet_group_name = "my_database_subnet_group"
+  		db_subnet_group_name = "db_sub_group"
   		parameter_group_name = "default.mysql5.6"
 	
-		tags = {
-			name = "myRDSinstance"
+		  tags = {
+			  name = "myRDSinstance"
 		}
 		
 		backup_retention_period = 7
-	}
+}
 	
-	output "rds_endpoint" {
+output "rds_endpoint" {
  		 value = aws_db_instance.default.endpoint
-	}
+}
 
-	output "rds_instance_id" {
+output "rds_instance_id" {
   		value = aws_db_instance.default.id
-	}
+}
 	
 	
 
